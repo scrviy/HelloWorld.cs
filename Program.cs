e7659e063d1ec9032d2e995095315a756114041e
scrviy <91909966+scrviy@users.noreply.github.com>
2023-02-17 10:48:31 -0300

    commitudo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program 
+    {
+        public static void Main(string[] args)
+        {
+            string frase = "Welcome to .NET ecossystem.";
+            Console.WriteLine(frase);
+        }
+    }
+}
